33279daa3f7ad59fccbb4ea80410991a91725399
gabrielmds222 <gabrielmedsilva@outlook.com>
2023-05-26 19:55:56 -0300

    feat: Integra Api do pokemon e mapeia lista de cards

---
+++ mobile/src/components/PokemonCard/index.tsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import {
+  Box,
+  AspectRatio,
+  Center,
+  Stack,
+  Heading,
+  HStack,
+  Image,
+  Text,
+  VStack,
+} from "native-base";
+import axios from "axios";
+
+interface Pokemon {
+  name: string;
+  image: string;
+  id: number;
+  types: string[];
+}
+
+const PokemonCard = ({ id, name, image, types }: Pokemon) => {
+  return (
+    <Box
+      w="160"
+      h="200"
+      rounded="lg"
+      overflow="hidden"
+      borderColor="coolGray.200"
+      m="5px"
+      borderWidth="1"
+      _dark={{
+        borderColor: "coolGray.600",
+        backgroundColor: "gray.700",
+      }}
+      _web={{
+        shadow: 2,
+        borderWidth: 0,
+      }}
+      _light={{
+        backgroundColor: "gray.50",
+      }}
+    >
+      <Box>
+        <AspectRatio w="100%" ratio={16 / 9}>
+          <Image
+            source={{ uri: image }}
+            alt={name}
+            style={{ width: 100, height: 100 }}
+          />
+        </AspectRatio>
+      </Box>
+      <Stack p="4" space={1}>
+        <Stack space={2}>
+          <Heading size="sm" ml="-1">
+            #{id.toString().padStart(2, "0")}
+          </Heading>
+          <Text fontSize="lg" fontWeight="500" ml="-0.5" mt="-1">
+            {name}
+          </Text>
+        </Stack>
+        <Text fontWeight="300">Tipo: {types.join(", ")}</Text>
+      </Stack>
+    </Box>
+  );
+};
+
+export default PokemonCard;
--- mobile/src/pages/Principal/index.tsx
+++ mobile/src/pages/Principal/index.tsx
@@ -1,20 +1,81 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { Box, Text, ScrollView } from "native-base";
+import { Box, VStack, View, FlatList } from "native-base";
+import axios from "axios";
+import PokemonCard from "../../components/PokemonCard";
+import Pesquisa from "../../components/Pesquisa";
+
+interface Pokemon {
+  name: string;
+  image: string;
+  id: number;
+  types: string[];
+}
 
 const Principal = () => {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+  async function getPokemons() {
+    try {
+      const response = await axios.get(
+        "https://pokeapi.co/api/v2/pokemon?limit=151"
+      );
+      const pokemonData = response.data.results;
+
+      const updatedPokemons = await Promise.all(
+        pokemonData.map(async (pokemon: { name: string; url: string }) => {
+          const pokemonResponse = await axios.get(pokemon.url);
+          const { id, types } = pokemonResponse.data;
+          const image = pokemonResponse.data.sprites.front_default;
+          return {
+            name: pokemon.name,
+            image: image,
+            id: id,
+            types: types.map(
+              (type: { type: { name: string } }) => type.type.name
+            ),
+          };
+        })
+      );
+
+      setPokemons(updatedPokemons);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getPokemons();
+  }, []);
+
   return (
-    <>
-      <Box bg="red.500" h={200} justifyContent="center" alignItems="center">
-        <Text>Opa</Text>
-      </Box>
-      <ScrollView flex={1}>
-        <Box bg="green.500" h={700} justifyContent="center" alignItems="center">
-          <Text>Conteudo</Text>
+    <View flex={1} bg="#27282D">
+      <Box h={200} justifyContent="center" alignItems="center">
+        <Box w="80%" justifyContent="center" mt="70px">
+          <Pesquisa />
         </Box>
-      </ScrollView>
+      </Box>
+
+      <Box h={700}>
+        <FlatList
+          data={pokemons}
+          numColumns={2}
+          renderItem={({ item }) => (
+            <Box mx={2} my={2} flex={1}>
+              <PokemonCard
+                id={item.id}
+                name={item.name}
+                image={item.image}
+                types={item.types}
+              />
+            </Box>
+          )}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      </Box>
+
       <StatusBar style="auto" />
-    </>
+    </View>
   );
 };
 
