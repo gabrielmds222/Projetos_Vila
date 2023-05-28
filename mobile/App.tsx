13de0eb3d70ae202b7568e76d7f12f57c87c483c
gabrielmds222 <gabrielmedsilva@outlook.com>
2023-05-28 16:39:30 -0300

    feat: Adiciona routes ao App.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import Principal from "./src/pages/Principal";
 import { NativeBaseProvider } from "native-base";
+import { Routes } from "./src/routes";
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <Principal />
+      <Routes />
     </NativeBaseProvider>
   );
 }
